ddf91ae8a316c47e2cf7638d7eb8025a7e954ebc
Ankitbhardwajj <82171833+Ankitbhardwajj@users.noreply.github.com>
2021-04-08 22:20:16 +0530

    Add files via upload

---
+++ Taking input.cpp
@@ -0,0 +1,11 @@
+using namespace std;
+#include<iostream>
+int main(){
+	int a,b;
+	cout<<"Enter the first number";
+	cin>>a;
+	cout<<"Enter the second number";
+	cin>>b;
+	cout<<"sum of a and b is "<<a+b;
+	return 0;
+}
